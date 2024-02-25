fb80599c4f5b2e045d6af1aba18ce563fc68351d
yngrtc <y@ngr.tc>
2024-02-25 10:01:50 -0800

    add sfu impl clap

--- examples/str0m_impl/mod.rs
+++ examples/str0m_impl/mod.rs
@@ -13,7 +13,70 @@ use str0m::channel::{ChannelData, ChannelId};
 use str0m::media::{Direction, KeyframeRequest, MediaData, Mid, Rid};
 use str0m::media::{KeyframeRequestKind, MediaKind};
 use str0m::net::Protocol;
-use str0m::{net::Receive, Event, IceConnectionState, Input, Output, Rtc};
+use str0m::{net::Receive, Event, IceConnectionState, Input, Output, Rtc, RtcError};
+
+/// This is the "main run loop" that handles all clients, reads and writes UdpSocket traffic,
+/// and forwards media data between clients.
+pub fn run_str0m(socket: UdpSocket, rx: Receiver<Rtc>) -> Result<(), RtcError> {
+    let mut clients: Vec<Client> = vec![];
+    let mut to_propagate: VecDeque<Propagated> = VecDeque::new();
+    let mut buf = vec![0; 2000];
+
+    loop {
+        // Clean out disconnected clients
+        clients.retain(|c| c.rtc.is_alive());
+
+        // Spawn new incoming clients from the web server thread.
+        if let Some(mut client) = spawn_new_client(&rx) {
+            // Add incoming tracks present in other already connected clients.
+            for track in clients.iter().flat_map(|c| c.tracks_in.iter()) {
+                let weak = Arc::downgrade(&track.id);
+                client.handle_track_open(weak);
+            }
+
+            clients.push(client);
+        }
+
+        // Poll clients until they return timeout
+        let mut timeout = Instant::now() + Duration::from_millis(100);
+        for client in clients.iter_mut() {
+            let t = poll_until_timeout(client, &mut to_propagate, &socket);
+            timeout = timeout.min(t);
+        }
+
+        // If we have an item to propagate, do that
+        if let Some(p) = to_propagate.pop_front() {
+            propagate(&p, &mut clients);
+            continue;
+        }
+
+        // The read timeout is not allowed to be 0. In case it is 0, we set 1 millisecond.
+        let duration = (timeout - Instant::now()).max(Duration::from_millis(1));
+
+        socket
+            .set_read_timeout(Some(duration))
+            .expect("setting socket read timeout");
+
+        if let Some(input) = read_socket_input(&socket, &mut buf) {
+            // The rtc.accepts() call is how we demultiplex the incoming packet to know which
+            // Rtc instance the traffic belongs to.
+            if let Some(client) = clients.iter_mut().find(|c| c.accepts(&input)) {
+                // We found the client that accepts the input.
+                client.handle_input(input);
+            } else {
+                // This is quite common because we don't get the Rtc instance via the mpsc channel
+                // quickly enough before the browser send the first STUN.
+                debug!("No client accepts UDP input: {:?}", input);
+            }
+        }
+
+        // Drive time forward in all clients.
+        let now = Instant::now();
+        for client in &mut clients {
+            client.handle_input(Input::Timeout(now));
+        }
+    }
+}
 
 /// Receive new clients from the receiver and create new Client instances.
 pub fn spawn_new_client(rx: &Receiver<Rtc>) -> Option<Client> {
