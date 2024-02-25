25a26bc3a6523c570455fc1512c89baa7c8c29d9
yngrtc <y@ngr.tc>
2024-02-25 00:51:16 -0800

    change port to 3478

--- examples/chat.rs
+++ examples/chat.rs
@@ -49,7 +49,7 @@ pub fn main() {
 
     // Spin up a UDP socket for the RTC. All WebRTC traffic is going to be multiplexed over this single
     // server socket. Clients are identified via their respective remote (UDP) socket address.
-    let socket = UdpSocket::bind(format!("{host_addr}:0")).expect("binding a random UDP port");
+    let socket = UdpSocket::bind(format!("{host_addr}:3478")).expect("binding a random UDP port");
     let addr = socket.local_addr().expect("a local socket adddress");
     info!("Bound UDP port: {}", addr);
 
