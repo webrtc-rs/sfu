c56d7e52aa1ad4e04c4a59d93ac25f428968a403
Rain Liu <y@liu.mx>
2026-07-03 21:50:38 -0700

    add RequestId type

--- src/event.rs
+++ src/event.rs
@@ -4,35 +4,38 @@ use rtc::peer_connection::transport::RTCIceCandidateInit;
 use crate::client::ClientId;
 use crate::room::RoomId;
 
+pub type RequestId = u64;
+
 #[allow(clippy::large_enum_variant)]
 #[derive(Debug)]
 pub enum Event {
     SessionDescription {
-        request_id: u64,
+        request_id: RequestId,
         room_id: RoomId,
         client_id: ClientId,
         sdp: RTCSessionDescription,
     },
     IceCandidate {
-        request_id: u64,
+        request_id: RequestId,
         room_id: RoomId,
         client_id: ClientId,
         candidate: RTCIceCandidateInit,
     },
     ClientConnected {
-        request_id: u64,
+        request_id: RequestId,
         room_id: RoomId,
         client_id: ClientId,
     },
     ClientDisconnected {
-        request_id: u64,
+        request_id: RequestId,
         room_id: RoomId,
         client_id: ClientId,
+        reason: String,
     },
     Error {
-        request_id: u64,
-        room_id: RoomId,
-        client_id: ClientId,
+        request_id: RequestId,
+        room_id: Option<RoomId>,
+        client_id: Option<ClientId>,
         reason: String,
     },
 }
